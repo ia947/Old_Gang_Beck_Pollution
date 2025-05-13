79dc1cbcf503bf896267554b54210cfd1d340bb5
Isaac Abbott <ia947@york.ac.uk>
2025-05-13 16:05:48 +0100

    Add dataframes as pandas dfs

--- old_gang_beck_metal_pollution_analysis.py
+++ old_gang_beck_metal_pollution_analysis.py
@@ -7,8 +7,14 @@ from geopy.distance import geodesic
 import SALib
 import PourPy as pb
 
+
 ################################
 ###### LOAD FILTERED DATA ######
 ################################
 
+ogb_df = pd.read_csv("old_gang_beck_processed_2025.csv")
+fw_specific_pollutants_EQS = pd.read_csv("freshwater_specific_pollutants_EQS_filtered.csv")
+fw_priority_hazardous_EQS = pd.read_csv("freshwater_priority_hazardous_EQS_filtered.csv")
+
+print(ogb_df)
 
