1ec242908e1424958fc34164e5fb72a80e4171b5
Isaac Abbott <ia947@york.ac.uk>
2025-05-13 16:01:43 +0100

    Add main code file for analysis

---
+++ old_gang_beck_metal_pollution_analysis.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+from scipy.optimize import minimize, curve_fit
+import matplotlib.pyplot as plt
+import seaborn as sns
+from geopy.distance import geodesic
+import SALib
+import PourPy as pb
+
+################################
+###### LOAD FILTERED DATA ######
+################################
+
+
