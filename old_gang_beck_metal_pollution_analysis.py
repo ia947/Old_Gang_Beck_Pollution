be53dfdb4191e63d9bf9a7e47ebde32d60e10aa3
Isaac Abbott <ia947@york.ac.uk>
2025-05-16 12:54:44 +0100

    Write complete function to load and process raw metal data, converting to micrograms and saving as new csv

--- old_gang_beck_metal_pollution_analysis.py
+++ old_gang_beck_metal_pollution_analysis.py
@@ -1,20 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 14 12:21:21 2025
+
+@author: isaac
+"""
 import numpy as np
 import pandas as pd
-from scipy.optimize import minimize, curve_fit
 import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.io.img_tiles as cimgt
 import seaborn as sns
-from geopy.distance import geodesic
-import SALib
-import PourPy as pb
+import os
+from SALib.sample import saltelli
+from SALib.analyze import sobol
+from tabulate import tabulate
+from matplotlib.colors import LinearSegmentedColormap, BoundaryNorm
 
+###############################
+###### DATA LOADING & PREP #####
+###############################
 
-################################
-###### LOAD FILTERED DATA ######
-################################
-
-ogb_df = pd.read_csv("old_gang_beck_processed_2025.csv")
-fw_specific_pollutants_EQS = pd.read_csv("freshwater_specific_pollutants_EQS_filtered.csv")
-fw_priority_hazardous_EQS = pd.read_csv("freshwater_priority_hazardous_EQS_filtered.csv")
-
-print(ogb_df)
-
+def load_and_process_metal_data():
+    """Load and preprocess all required datasets"""
+    # Load main data
+    ogb_raw_df = pd.read_excel("OGB_raw_no_repeat.xlsx")
+    
+    # Identify metal columns (columns G to AC in the original data)
+    metal_columns = ogb_raw_df.columns[6:29]
+    
+    # Convert metal values from mg/L to µg/L
+    ogb_raw_df[metal_columns] = ogb_raw_df[metal_columns] * 1000
+    
+    # Save the results (optional)
+    ogb_raw_df.to_csv('converted_metals_micrograms.csv', index=False)
+    
+    # Show confirmation
+    print("Conversion complete. Columns converted:")
+    print(list(metal_columns))
+    
+    
+    return ogb_raw_df
